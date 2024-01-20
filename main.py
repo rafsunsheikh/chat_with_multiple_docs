87f8db4ba77e8680a6c616b3809a0e89e303ccc8
rafsunsheikh <201614123@student.mist.ac.bd>
2024-01-20 08:34:33 +0600

    added requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
